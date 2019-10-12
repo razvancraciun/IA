71b3bef8a7099f7e2a07c19d08a41c043510a66f
Razvan Craciun <razvancraciun9851@gmail.com>
2019-10-12 20:34:06 +0300

    Added IDDFS

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import argparse
 from game import Missionaries_and_canibals
 from strategies import Random_Strategy
 from strategies import Backtracking_Strategy
+from strategies import IDDFS_Strategy
+from time import time
 
-def main():
-    # game = Missionaries_and_canibals(3,3,2)
-    game = Missionaries_and_canibals(3,3,2)
-    strategy = Backtracking_Strategy(game)
-    strategy.run()
+ap = argparse.ArgumentParser()
+ap.add_argument('-m', '--missionaries', type = int, required = True)
+ap.add_argument('-c', '--cannibals', type = int, required = True)
+ap.add_argument('-b', '--boat_size', type = int, required = True)
+args = ap.parse_args()
+
+def main(missionaries, cannibals, boat_size):
+    game = Missionaries_and_canibals(missionaries,cannibals,boat_size)
+    random_strategy = Random_Strategy(game)
+    bkt_strategy = Backtracking_Strategy(game)
+    iddfs_strategy = IDDFS_Strategy(game)
+
+    before_bkt = time()
+    bkt_strategy.run()
+    print(f'Bkt took: + {time() - before_bkt}')
+
+    before_iddfs = time()
+    iddfs_strategy.run()
+    print(f'IDDFS took: {time() - before_iddfs}')
+
 
 if __name__ == '__main__':
-    main()
+    main(args.missionaries, args.cannibals, args.boat_size)
--- strategies.py
+++ strategies.py
@@ -1,5 +1,61 @@
 import random
 
+class IDDFS_Strategy:
+    def __init__(self, game, depth_step = 3):
+        self.game = game
+        self.visited = []
+        self.parent = {}
+        self.depth = {}
+        self.depth_step = depth_step
+
+    def initialize(self):
+        self.game.reset()
+        self.visited = []
+        self.parent = {}
+        self.depth = {}
+        self.depth[self.game.state] = 0
+        self.parent[self.game.state] = None
+
+
+    def trace(self):
+        trace = []
+        state = self.game.state
+        while self.parent[state]:
+            trace.append(state)
+            state = self.parent[state]
+        for state in reversed(trace):
+            print(state)
+        print(f'Solution passes through {len(trace)} states')
+
+    def run(self):
+        self.initialize()
+        shallow = [self.game.state]
+        while shallow:
+            shallow = self.step(shallow)
+        self.game.game_over()
+
+
+    def step(self, shallow):
+        deep = []
+        while shallow:
+            self.game.state = shallow.pop()
+            self.visited.append(self.game.state)
+            possible_transitions = self.game.possible_transitions()
+            possible_transitions = [transition for transition in possible_transitions if transition not in self.visited]
+            possible_transitions = [transition for transition in possible_transitions \
+                                if transition not in shallow and transition not in deep]
+            for transition in possible_transitions:
+                self.depth[transition] = self.depth[self.game.state] + 1
+                self.parent[transition] = self.game.state
+                if self.depth[transition] < self.depth_step:
+                    shallow.append(transition)
+                else:
+                    deep.append(transition)
+                if self.game.is_final_state():
+                    self.trace()
+                    return []
+        return deep
+
 class Backtracking_Strategy:
     def __init__(self, game):
         self.game = game
@@ -33,6 +89,8 @@ class Backtracking_Strategy:
             state = self.parent[state]
         for state in reversed(trace):
             print(state)
+        print(f'Solution passes through {len(trace)} states')
+
 
 
     def initialize(self):
